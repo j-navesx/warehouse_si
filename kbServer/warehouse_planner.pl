1eb3cf685354e66a40173e927fef66e77d24ac96
j-navesx <j.naves@campus.fct.unl.pt>
2021-11-09 16:37:55 +0000

    ignore added + planner y gotos finished

--- kbServer/warehouse_planner.pl
+++ kbServer/warehouse_planner.pl
@@ -54,18 +54,25 @@ strips([
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                          Y Movement                                  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
 
-%// TODO:check what y corresponds to inside and outside (Ctrl+H IN and OUT bellow)
 strips([
-    act     [subplan(goto_y_in, [move_y_in, wait_until(y_is_at(IN)), stop_y])]
-    pre     [y_is_at(OUT), y_moving("Not Moving.")],
-    add     [y_is_at(IN)],
-    del     [y_is_at(OUT)]    
+act     [subplan(goto_y_in, [move_y_out, wait_until(y_is_at(1)), stop_y])]
+pre     [y_is_at(Yi), y_moving("Not Moving.")],
+add     [y_is_at(1)],
+del     [y_is_at(Yi)]    
 ]).
 
 strips([
-    act     [subplan(goto_y_out, [move_y_out, wait_until(y_is_at(OUT)), stop_y])]
-    pre     [y_is_at(IN), y_moving("Not Moving.")],
-    add     [y_is_at(OUT)],
-    del     [y_is_at(IN)]    
+    act     [subplan(goto_y_center, [move_y_in, wait_until(y_is_at(2)), stop_y])]
+    pre     [y_is_at(Yi), y_moving("Not Moving.")],
+    add     [y_is_at(2)],
+    del     [y_is_at(Yi)]    
+]).
+
+strips([
+act     [subplan(goto_y_out, [move_y_out, wait_until(y_is_at(3)), stop_y])]
+pre     [y_is_at(Yi), y_moving("Not Moving.")],
+add     [y_is_at(3)],
+del     [y_is_at(Yi)]    
 ]).
