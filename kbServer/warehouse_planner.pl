9245634eeac8b9d2294e38a1ece933911eea673c
j-navesx <j.naves@campus.fct.unl.pt>
2021-11-21 12:05:08 +0000

    Done all todo's strip in planner (awaiting confirmation)

--- kbServer/warehouse_planner.pl
+++ kbServer/warehouse_planner.pl
@@ -90,12 +90,39 @@ strips([
 
 % // TODO: INPUT BOX FROM LEFT STATION
 strips([
-    act     [subplan(get_box, [move_y_in, wait_unitl(y_is_at(1)), move_z_up, wait_unitl(z_is_at(1.5)), move_y_out, wait_unitl(y_is_at(2))])],
+    act     [subplan(get_box, [move_y_out, wait_unitl(y_is_at(1)), move_z_up, wait_unitl(z_is_at(1.5)), move_y_out, wait_unitl(y_is_at(2))])],
     pre     [left_station_moving(0), part_at_left_station, x_is_at(1), z_is_at(1), y_is_at(2)],
     add     [part_in_cage],
     del     [part_at_left_station]    
 ]).
+
 % // TODO: INSERT BOX IN CELL
+strips([
+    act     [subplan(place(X,Z), [move_y_in, wait_unitl(y_is_at(3)), move_z_down, wait_until(z_is_at(Z-0.5)), move_y_out, wait_unitl(y_is_at(2))])],
+    pre     [part_in_cage, x_is_at(X), z_is_at(Z)],
+    add     [cell(X,Z)],
+    del     [part_in_cage]    
+]).
+
 % // TODO: REMOVE BOX FROM CELL
+strips([
+    act     [subplan(remove(X,Z), [move_y_in, wait_unitl(y_is_at(3)), move_z_up, wait_unitl(z_is_at(Z)), move_y_out, wait_unitl(y_is_at(2))])],
+    pre     [cell(X,Z), x_is_at(X), z_is_at(Z-0.5)],
+    add     [part_in_cage],
+    del     [cell(X,Z)]    
+]).
+
 % // TODO: OUTPUT BOX ON RIGHT STATION
+strips([
+    act     [subplan(deliver_box, [move_y_out, wait_until(y_is_at(1)), move_z_down, wait_unitl(z_is_at(1)), move_y_in, wait_unitl(y_is_at(2))])],
+    pre     [part_in_cage, x_is_at(10), z_is_at(1.5)],
+    add     [part_at_right_station],
+    del     [part_in_cage]    
+]).
 % // TODO: CLEAR RIGHT STATION
+strips([
+    act     [subplan(deliver, [move_right_station_out, sleep(1), stop_right_station])],
+    pre     [right_station_moving(0)],
+    add     [],
+    del     [part_at_right_station]    
+]).
